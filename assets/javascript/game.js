985b8790d4eace4ddc4acfe05e03bb2e4ca6b630
Orpa <orski_76@yahoo.com>
2020-05-06 06:02:58 -0400

    changed "goal" return

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,19 +6,18 @@ let counter2 = document.querySelector ("#teamtwo-numshots");
 let randomOne = document.querySelector ("#teamone-numgoals");
 let randomTwo = document.querySelector ("#teamtwo-numgoals");
 let numberOfReset = document.querySelector("#num-resets");
+
 resetButton.addEventListener("click",function()
 {
     console.log("+ button clicked");
 let NewReset = Number(numberOfReset.innerHTML) + 1;
 numberOfReset.innerHTML=NewReset;
 
-
 document.getElementById("teamone-numshots").innerHTML="0"; randomOne.style.color="black";
 document.getElementById("teamtwo-numshots").innerHTML="0";randomTwo.style.color="black";
 document.getElementById("teamone-numgoals").innerHTML="0";counter1.style.color="black";
 document.getElementById("teamtwo-numgoals").innerHTML="0";counter2.style.color="black";
 })
-
 
 teamOneshot.addEventListener("click",function()
 {
@@ -27,22 +26,20 @@ let newCountshot = Number(counter1.innerHTML) + 1;
 counter1.innerHTML = newCountshot;
 let random1 = Math.floor(Math.random()*10)+1;
 console.log(random1)
-let goalShot=Number(randomOne.innerHTML)
+
 if (random1 <= 5){
     randomOne.innerHTML = 
     1;
-    goalShot =goalShot +1;
-
-
+    randomOne.innerHTML = "GOAL";
+        randomOne.style.color= "blue"
     }
 
     else {
         randomOne.innerHTML = "FAIL";
         randomOne.style.color= "red"
     }
-
-
 })
+
 teamTwoshot.addEventListener("click",function()
 {
     console.log("+ button clicked");
@@ -52,9 +49,7 @@ let random2 = Math.floor(Math.random()*10)+1;
 console.log(random2)
 if (random2 <= 5){
     randomTwo.innerHTML = "GOAL";
-    randomTwo.style.color= "blue"}
-
-     
+    randomTwo.style.color= "blue"}   
 
     else {
         randomTwo.innerHTML = "FAIL";
